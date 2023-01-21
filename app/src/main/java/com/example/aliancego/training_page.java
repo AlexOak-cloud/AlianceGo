28a36a95f9bc95fb33b2a7e2ee1a94ef2d57de3d
AlexOak <dubovec995@gmail.com>
2023-01-21 20:09:28 +0100

    create_project

---
+++ app/src/main/java/com/example/aliancego/training_page.java
@@ -0,0 +1,21 @@
+package com.example.aliancego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class training_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_page);
+    }
+
+
+    }
+
+
+
