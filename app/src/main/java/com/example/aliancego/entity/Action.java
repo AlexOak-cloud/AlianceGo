69a2a083a71080e0973084e090fac7ee011347f7
AlexOak <dubovec995@gmail.com>
2023-01-23 00:27:36 +0100

    create

---
+++ app/src/main/java/com/example/aliancego/entity/Action.java
@@ -0,0 +1,100 @@
+package com.example.aliancego.entity;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Action {
+
+    private int id;
+    private String name;
+    private String description;
+    private String path;
+    private int time;
+    private File gif;
+
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", path='" + path + '\'' +
+                ", time=" + time +
+                ", gif=" + gif +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Action entity = (Action) o;
+        return id == entity.id && time == entity.time && Objects.equals(name, entity.name) && Objects.equals(description, entity.description) && Objects.equals(path, entity.path) && Objects.equals(gif, entity.gif);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, path, time, gif);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public File getGif() {
+        return gif;
+    }
+
+    public void setGif(File gif) {
+        this.gif = gif;
+    }
+
+    public Action() {
+    }
+
+    public Action(int id, String name, String description, String path, int time, File gif) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.path = path;
+        this.time = time;
+        this.gif = gif;
+    }
+}
---
+++ app/src/main/java/com/example/aliancego/entity/Workout.java
@@ -0,0 +1,89 @@
+package com.example.aliancego.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+public class Workout {
+
+    private int id;
+    private String name;
+    private int time;
+    private LocalDateTime date;
+    private List<Action> actions;
+
+    @Override
+    public String toString() {
+        return "Workout{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", time=" + time +
+                ", date=" + date +
+                ", actions=" + actions +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Workout workout = (Workout) o;
+        return id == workout.id && time == workout.time && Objects.equals(name, workout.name) && Objects.equals(date, workout.date) && Objects.equals(actions, workout.actions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, time, date, actions);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(List<Action> actions) {
+        this.actions = actions;
+    }
+
+    public Workout() {
+    }
+
+    public Workout(int id, String name, int time, LocalDateTime date, List<Action> actions) {
+        this.id = id;
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.actions = actions;
+    }
+}
