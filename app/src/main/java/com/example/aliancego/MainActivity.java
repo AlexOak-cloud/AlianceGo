578903529e46778814c541dd0841b14748f477a7
AlexOak <dubovec995@gmail.com>
2023-01-29 00:53:21 +0100

    qw

--- app/src/main/java/com/example/aliancego/MainActivity.java
+++ app/src/main/java/com/example/aliancego/MainActivity.java
@@ -1,47 +1,62 @@
 package com.example.aliancego;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button signIn;
-    private Button registration;
+    private Button signIn, buttonRegistration;
+    FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /** Кнопка входа -> */
-      signIn = findViewById(R.id.button_input);
 
-        signIn.setOnClickListener(new View.OnClickListener() {
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        buttonRegistration = findViewById(R.id.button_registration);
+        buttonRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, InputActivity.class);
-                startActivity(intent);
+                showRegisterWindow();
             }
         });
 
-        /**  Кнопка регистрации -> */
+//
+    }
 
-        registration = findViewById(R.id.button_registration);
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("register");
+        dialog.setMessage("input all data for registration");
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.activity_registration, null);
+        dialog.setView(registerWindow);
+        
 
-        registration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
 
 
     }
+
     public void butClick(View v) {
         Intent intent = new Intent(this, WorkActivity.class);
         startActivity(intent);
