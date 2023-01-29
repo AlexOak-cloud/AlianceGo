2931ca533c17ed2a820f1a57c9bcfc310755a040
AlexOak <dubovec995@gmail.com>
2023-01-29 21:11:52 +0100

    qw

--- app/src/main/java/com/example/aliancego/MainActivity.java
+++ app/src/main/java/com/example/aliancego/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.aliancego;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
+import com.example.aliancego.entity.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,20 +28,33 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button signIn, buttonRegistration;
-    FirebaseAuth auth;
+    private Button buttonSignIn, buttonRegistration;
+    private FirebaseAuth auth;
     private FirebaseDatabase db;
     private DatabaseReference users;
 
+    RelativeLayout root;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        root = findViewById(R.id.root_element);
 
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance();
         users = db.getReference("Users");
+
+        buttonSignIn = findViewById(R.id.button_signIn);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignInWindow();
+            }
+        });
 
         buttonRegistration = findViewById(R.id.button_registration);
         buttonRegistration.setOnClickListener(new View.OnClickListener() {
@@ -43,18 +67,117 @@ public class MainActivity extends AppCompatActivity {
 //
     }
 
+    private void showSignInWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("sign in");
+        dialog.setMessage("input all data for sign in");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.activity_input, null);
+        dialog.setView(registerWindow);
+
+        EditText password = findViewById(R.id.editPassword);
+        EditText email = findViewById(R.id.editEmail);
+
+        dialog.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+        dialog.setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Input email", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().length() < 5) {
+                    Snackbar.make(root, "Input password ( password > 5 )", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new  Intent(MainActivity.this, MapActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(root, "false " + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+        dialog.show();
+    }
+
     private void showRegisterWindow() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("register");
         dialog.setMessage("input all data for registration");
+
         LayoutInflater inflater = LayoutInflater.from(this);
         View registerWindow = inflater.inflate(R.layout.activity_registration, null);
         dialog.setView(registerWindow);
-        
+
+        EditText name = findViewById(R.id.editName);
+        EditText password = findViewById(R.id.editPassword);
+        EditText email = findViewById(R.id.editEmail);
+
+        dialog.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
 
 
+        dialog.setPositiveButton("Registration", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
 
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Input email", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(name.getText().toString())) {
+                    Snackbar.make(root, "Input name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText().toString().length() < 5) {
+                    Snackbar.make(root, "Input password ( password > 5 )", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
 
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User(name.getText().toString(),
+                                        password.getText().toString(),
+                                        email.getText().toString());
+
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()    )
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Snackbar.make(root, "Created!", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+        dialog.show();
     }
 
     public void butClick(View v) {
---
+++ app/src/main/java/com/example/aliancego/entity/User.java
@@ -0,0 +1,63 @@
+package com.example.aliancego.entity;
+
+import java.util.Objects;
+
+public class User {
+
+    private String name, password, email;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
