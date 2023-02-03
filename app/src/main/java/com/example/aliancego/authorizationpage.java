2b4e97266e6492f8d4b4e3676e0053379bcf41e0
admin <djek.oak@gmail.com>
2023-02-03 14:15:30 +0300

    DataUpdate

---
+++ app/src/main/java/com/example/aliancego/authorizationpage.java
@@ -0,0 +1,14 @@
+package com.example.aliancego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class authorizationpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorizationpage);
+    }
+}
